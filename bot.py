4fc574a5855caa1f2bfb23108f17d0279ba46a0b
qpjlygujlkOo <qpjlygujlkOo@gmail.com>
2024-04-11 14:21:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -190,7 +190,7 @@ def meetup(message):
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if (message.from_user.username == aza) and (message.text.casefold() in lunch_synonyms):
-        exp = open('gif/exp.gif', 'rb')
+        exp = open('gif/exp3.gif', 'rb')
         bot.send_message(message.chat.id, f'ب الهناء والشفاء / بالهنا والشف!, {message.from_user.first_name}!')
         bot.send_animation(message.chat.id, exp)
     elif (message.from_user.username == igor2) and (message.text.casefold() in lunch_synonyms):
