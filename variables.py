f07805effa4f18c4f48df01179bd6464d346d50f
qpjlygujlkOo <qpjlygujlkOo@gmail.com>
2024-11-05 10:34:54 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,7 +1,7 @@
 qa1 = '@lexm18, @Igor_Kirichek, @Vint_are_ez, @from_time_to_time, @lena596, @Azamat_Gu'
 tl1 = '@oblivantseva'
 tlqa = '@gryzb1'
-back1 = '@barysh_vn, @AKS2001, @DmitryChernoyarov, @MaximB98'
+back1 = '@barysh_vn, @AKS2001, @DmitryChernoyarov, @MaximB98, @mmeiko'
 front1 = '@chimir, @a2aev, @grigorevaleriia'
 pm1 = '@agantts, @Becky_Bones, @OlgaStupenkova'
 aqa1 = '@Azamat_Gu'
